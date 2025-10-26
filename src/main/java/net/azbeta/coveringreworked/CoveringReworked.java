4bb3a076f5518f959a1279e60f707fb518347237
AzBeta <4bet41sherebud@gmail.com>
2025-10-25 19:18:15 -0500

    Initial commit

---
+++ src/main/java/net/azbeta/coveringreworked/CoveringReworked.java
@@ -0,0 +1,16 @@
+package net.azbeta.coveringreworked;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CoveringReworked implements ModInitializer {
+	public static final String MOD_ID = "coveringreworked";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/azbeta/coveringreworked/CoveringReworkedClient.java
@@ -0,0 +1,10 @@
+package net.azbeta.coveringreworked;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CoveringReworkedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
